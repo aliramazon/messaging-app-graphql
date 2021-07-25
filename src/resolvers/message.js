e329a8ef15778a7cafe1a3a33c7ad0ec9e41f176
Ali Ramazon <mr.safarali@gmail.com>
2021-07-24 22:46:50 -0600

    Add pageInfo to handle cursor and hasNextpage

--- src/resolvers/message.js
+++ src/resolvers/message.js
@@ -2,6 +2,9 @@ import { combineResolvers } from "graphql-resolvers";
 import { Sequelize } from "sequelize";
 import { isAuthenticated, isMessageOwner } from "./auth";
 
+const encodeCursor = (string) => Buffer.from(string).toString("base64");
+const decodeCursor = (string) =>
+    Buffer.from(string, "base64").toString("ascii");
 const messageResolvers = {
     Query: {
         messages: async (parent, { cursor, limit = 100 }, { models }) => {
@@ -9,16 +12,29 @@ const messageResolvers = {
                 ? {
                       where: {
                           createdAt: {
-                              [Sequelize.Op.lt]: cursor
+                              [Sequelize.Op.lt]: decodeCursor(cursor)
                           }
                       }
                   }
                 : {};
-            return await models.Message.findAll({
+            const messages = await models.Message.findAll({
                 order: [["createdAt", "DESC"]],
-                limit,
+                limit: limit + 1,
                 ...cursorOptions
             });
+            let hasNextPage = messages.length > limit;
+            let edges = hasNextPage ? messages.slice(0, -1) : messages;
+            return {
+                edges,
+                pageInfo: {
+                    endCursor: hasNextPage
+                        ? encodeCursor(
+                              edges[edges.length - 1].createdAt.toString()
+                          )
+                        : null,
+                    hasNextPage
+                }
+            };
         },
         message: async (parent, { id }, { models }) => {
             return await models.Message.findByPk(id);
--- src/schema/message.js
+++ src/schema/message.js
@@ -2,7 +2,7 @@ import { gql } from "apollo-server-express";
 
 const messageSchema = gql`
     extend type Query {
-        messages(cursor: String, limit: Int): [Message!]!
+        messages(cursor: String, limit: Int): MessageConnection!
         message(id: ID!): Message!
     }
 
@@ -10,6 +10,16 @@ const messageSchema = gql`
         createMessage(text: String!): Message!
         deleteMessage(id: ID!): Boolean!
         updateMessage(id: ID!, text: String!): Message!
+    }
+
+    type MessageConnection {
+        edges: [Message!]!
+        pageInfo: PageInfo!
+    }
+
+    type PageInfo {
+        endCursor: String
+        hasNextPage: Boolean!
     }
 
     type Message {
